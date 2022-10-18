a69f33fb39ead90425472b3d49beb0626116ab7e
SemperOli github <royaloliverk@gmail.com>
2022-10-18 04:11:43 -0700

    Task 8 2nd Commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly and that piece of art is useful 
- * 
+ * main - prints exactly and that piece of art is useful  
  * Return: Always 1 (success)
 */
-
 int main(void)
 {
-  	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-  	return (1);
+        write(2, "and that piece of art is useful
+       	    \" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
 }
