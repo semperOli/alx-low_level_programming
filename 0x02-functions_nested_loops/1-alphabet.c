b286158653be56c5ae032389b9efe3b8784c6ceb
SemperOli github <royaloliverk@gmail.com>
2022-10-19 05:18:29 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - This prints the alphabet in lower case
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char lca;
+	for (lca = 'a'; lca <= 'z'; lca++)
+		_putchar(lca);
+
+	_putchar('\n');
+}
