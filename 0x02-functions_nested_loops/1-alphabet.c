0a070114ccdb4e8ae4b759f75068d5f243193bf8
SemperOli github <royaloliverk@gmail.com>
2022-10-19 18:46:14 -0700

    Task 3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_alphabet - This prints the alphabet in lower case
- * Return: Always 0.
+ * print_alphabet - Prints all letters of the alphabet 
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: 0
  */
 void print_alphabet(void)
 {
 	char lca;
-	for (lca = 'a'; lca <= 'z'; lca++)
-		_putchar(lca);
 
+	for (lca = 'a'; lca <= 'z'; i++)
+		_putchar(lca);
 	_putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - This prints the alphabet 10 times
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+char lca;
+int a;
+
+for (a = 0; a <= 9; a++)
+{
+for (lca = 'a'; lca <= 'z'; i++)
+_putchar(lca);
+_putchar('\n');
+}
+}
