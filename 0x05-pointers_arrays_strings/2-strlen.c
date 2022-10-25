023f7046f111a41204b9e33d13e293777ad53816
SemperOli <royaloliverk@gmail.com>
2022-10-25 05:10:25 -0700

    Task 2 update 4

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,14 +7,13 @@
  */
 int _strlen(char *s)
 {
-	int i = 1, length = 0;
-	char pl = s[0];
-
-	while (pl != '\0')
-	{
-		length++;
-		pl = s[i++];
-	}
-	return (length);
+int i = 1, length = 0;
+char pl = s[0];
+while (pl != '\0')
+{
+length++;
+pl = s[i++];
+}
+return (length);
 }
 
