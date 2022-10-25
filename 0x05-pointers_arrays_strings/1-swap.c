0878c32163fe4dda74dd29344566a165b189f27d
SemperOli <royaloliverk@gmail.com>
2022-10-25 04:36:32 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - a function for swapping integer values
+ * @a: First input
+ * @b: Second input
+ * Return: ints
+ */
+void swap_int(int *a, int *b)
+{
+int n;
+*a = n;
+*b = n;
+*b = *a;
+}
