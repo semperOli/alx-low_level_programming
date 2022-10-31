4bf4cdc93dc1a163261dd403bafa486fa5dc7003
SemperOli <royaloliverk@gmail.com>
2022-10-30 19:47:12 -0700

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,44 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _printstr - prints a given string
- * @c: a null temrinated character array
- *
- * Return: 0 if successful
- */
-int _printstr(char c[])
-{
-	int i = 0;
-
-	while (c[i] != '\0')
-		_putchar(c[i++]);
-
-	return (0);
-}
-
-/**
- * print_times_table - prints out an arbitrary multiplication table between
- * 1 and 15
- *
- * @n: integer that defines dimnsions for the table
- *
- * Return void
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
  */
 void print_times_table(int n)
 {
-	char buffer[5] = ",    ";
-	int i, j, num;
+	int num, mult, prod;
 
-	if (n < 0 || n > 15)
-		return;
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		_putchar('0');
-		for (j = 1; j <= n; j++)
+		for (num = 0; num <= n; num++)
 		{
-			num = i * j;
-			buffer[2] = num / 100 + 48;
-			buffer[3] = num / 10 % 10 + 48;
-			buffer[4] = num % 10 + 48;
-			if (buffer[2] == '0')
-				buffer[2] = ' ';
-			if (buffer[3] == '0' && buffer[2] == ' ')
-				buffer[3] = ' ';
-			_printstr(buffer);
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-}
+}}
