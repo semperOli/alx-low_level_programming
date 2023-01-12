a3f2dc04e5029c6277334f7d6df86bd02706bcb4
SemperOli <royaloliverk@gmail.com>
2023-01-12 00:32:27 -0800

    Task 5 inserted functions

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,29 @@
 #include "main.h"
+
 /**
- * _sqrt_recursion - square root of a number
+ * sqrt2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
  *
- * @n: int
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt2(a, b + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of n
+ * @n: Number Integer
  *
- * Return: integer
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _sqrt_recursion(int n)
 {
-	return (0);
+	return (sqrt2(n, 1));
 }
